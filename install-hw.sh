91bda7aa4b0f72c27dd4a11112f83dd74086dd9c
harleylara <harley.lara@outlook.com>
2026-07-17 00:05:55 +0200

    init hardware from source script

---
+++ install-hw.sh
@@ -0,0 +1,30 @@
+mkdir -p ~/foxy_ws/src
+cd ~/foxy_ws/src
+
+git clone https://github.com/EOLab-HSRW/foxy-robot.git
+
+cd ~/foxy_ws
+vcs import src --shallow < ~/foxy_ws/src/foxy-robot/hw.repos
+
+# required for gscam
+sudo apt install --no-install-recommends \
+  libgstreamer1.0-dev \
+  libgstreamer-plugins-base1.0-dev \
+  gstreamer1.0-tools
+
+source /opt/ros/humble/setup.bash
+sudo apt update
+
+rosdep install \
+  --simulate \
+  --from-paths src/gscam \
+  --ignore-src
+
+colcon build \
+  --packages-select gscam \
+  --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+rosdep install -r -y --ignore-src \
+  --from-paths $(colcon list --paths-only --packages-up-to foxy_bringup_hw)
+
+colcon build --packages-up-to foxy_bringup_hw
